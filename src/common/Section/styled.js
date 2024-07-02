f4541a15f5ef4c8f5b68f52d62be4d22c418c1d0
Dawid Galeski <dawidgaski@gmail.com>
2024-07-02 22:32:11 +0200

    added new css in section and created Headline

--- src/common/Section/styled.js
+++ src/common/Section/styled.js
@@ -1,6 +1,15 @@
 import styled from "styled-components";
 
 export const Section = styled.section`
-  max-width: 1216px;
+  max-width: 1368px;
+  max-height: 1391px;
   padding: 32px;
+  display: grid;
+  grid-gap: 24px;
+  grid-template-columns: auto;
+`;
+
+export const Headline = styled.h1`
+  font-weight: 600;
+  font-size: 36px;
 `;
