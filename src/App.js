59a3ee1e355e99f0de4641e7e6299b421da087d6
Adam Nowak <nowak.adam2222@gmail.com>
2024-07-03 21:18:33 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import MovieCard from "./common/MovieCard";
+
 function App() {
     return (
         <div className="App">
-            Adam Nowak
+            <MovieCard imageURL={"err"} title={"Mulan"} subtitle={"2023"} tags={["asdf", "gfds", "YYYT"]} rating={7.5} voteCount={14}/>
         </div>
     );
 }
---
+++ src/common/MovieCard/styled.js
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const CardContainer = styled.div`
+    height: 650px;
+    width: 324px;
+`
+
+export const Image = styled.img`
+
+`
+
+export const Info = styled.div`
+
+`
+
+export const Title = styled.p`
+
+`
+
+export const Subtitle = styled.p`
+
+`
+
+export const Tag = styled.div`
+
+`
+
+export const Rating = styled.p`
+    
+`
+
+export const VoteCount = styled.p`
+    
+`
