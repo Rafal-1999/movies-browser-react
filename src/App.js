050113a61ebf8a929ea9a2961145ddb326099ef1
Adam Nowak <nowak.adam2222@gmail.com>
2024-07-04 22:46:11 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import { useSelector } from "react-redux";
-import { selectMovies } from "./moviesSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { selectMovies, setMovies } from "./moviesSlice";
 import MovieCard from "./common/MovieCard";
+import { useMoviesData } from "./useMoviesData";
 
 function App() {
+    const dispatch = useDispatch();
+    dispatch(setMovies(useMoviesData()))
     const movies = useSelector(selectMovies);
     return (
         <div className="App">
             {movies.map(movie => (
-                <MovieCard imageURL={movie.imageUrl} title={movie.title} subtitle={movie.subtitle} tags={movie.tags} rating={movie.rating} voteCount={movie.voteCount}/>
+                <MovieCard imageURL={movie.imageUrl} title={movie.title} subtitle={movie.release_date} tags={movie.genre_ids} rating={movie.vote_average} voteCount={movie.vote_count}/>
             ))}
         </div>
     );
--- src/moviesSlice.js
+++ src/moviesSlice.js
@@ -2,24 +2,19 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const moviesSlice = createSlice({
     name: 'movies',
-    initialState: [
-        {
-            title: "mulan",
-            imageUrl: "https://s3-alpha-sig.figma.com/img/9516/5f66/20d05404c88db613b35de151d415cd32?Expires=1721001600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=EO3xUr3QiB4YgbVcp6IHkNcgXVwGhhHX4UJD~jM3ubg-odS8Aw8XhgsmAAJhuB8fy76d1qQMx3Nsiiq-ONEHpAjk6XFJ2LiN8akDt0M8OQQdsPTj4fJ-7~LlOkLl79sJeq-~9S1H1Q3aUAsivNLt9iaVR~jWGB84qHiH5X9zR0QY0KCe50jDd5fM8P-m8KFBZ5M8qBnxiGqgmF38W4WQDCZc~lXvYQdmmx6ymkh3NrPL7OM~fC0u8AHnp~pv6zV7RgW~KCU0ntsfNV7PjEJeu-pPdVLjxcGNgffGNm4yYps2WxjQarfkK86XY1THYIcBV~28lnPSNf-nkl5kjBSBpA__",
-            subtitle: "2022",
-            tags: ["asfdds", "asdf"],
-            rating: 7.5,
-            voteCount: 23,
-        }
-    ],
+    initialState: {
+        movieList: []
+    },
     reducers: {
-        setMovies: ({state, payload}) => {
-            state.value = payload.value;
-        },
+        setMovies: (state, {payload}) => {
+            if (payload.state != "loading") {
+                state.movieList = payload.results;
+            }
+        }
     },
 });
 
 export const { setMovies } = moviesSlice.actions;
-export const selectMovies = state => state.movies;
+export const selectMovies = state => state.movies.movieList;
 
 export default moviesSlice.reducer
