d6f3a67ae585f18f0eeb81c715a81f85b07248d1
Adam Nowak <nowak.adam2222@gmail.com>
2024-07-05 20:37:41 +0200

    genre name display

--- src/App.js
+++ src/App.js
@@ -9,25 +9,25 @@ function App() {
     const movies = useSelector(selectMovies);
 
     const genreTags = [
-        {name: "Action", id: 28},
-        {name: "Adventure", id: 12},
-        {name: "Animation", id: 16},
-        {name: "Comedy", id: 35},
-        {name: "Crime", id: 80},
-        {name: "Documentary", id: 99},
-        {name: "Drama", id: 18},
-        {name: "Family", id: 10751},
-        {name: "Fantasy", id: 14},
-        {name: "History", id: 36},
-        {name: "Horror", id: 27},
-        {name: "Music", id: 10402},
-        {name: "Mystery", id: 9648},
-        {name: "Romance", id: 10749},
-        {name: "Sci-Fi", id: 878},
-        {name: "TV Movie", id: 10770},
-        {name: "Thriller", id: 53},
-        {name: "War", id: 10752},
-        {name: "Western", id: 37},
+        { name: "Action", id: 28 },
+        { name: "Adventure", id: 12 },
+        { name: "Animation", id: 16 },
+        { name: "Comedy", id: 35 },
+        { name: "Crime", id: 80 },
+        { name: "Documentary", id: 99 },
+        { name: "Drama", id: 18 },
+        { name: "Family", id: 10751 },
+        { name: "Fantasy", id: 14 },
+        { name: "History", id: 36 },
+        { name: "Horror", id: 27 },
+        { name: "Music", id: 10402 },
+        { name: "Mystery", id: 9648 },
+        { name: "Romance", id: 10749 },
+        { name: "Sci-Fi", id: 878 },
+        { name: "TV Movie", id: 10770 },
+        { name: "Thriller", id: 53 },
+        { name: "War", id: 10752 },
+        { name: "Western", id: 37 },
     ];
 
 
@@ -35,9 +35,12 @@ function App() {
     return (
         <div className="App">
             {movies.map(movie => {
-                console.log(genreTags.find((tag) => movie.genre_ids.includes(tag.id)).name)
+                let movieTags = [];
+                movie.genre_ids.forEach(id => {
+                    movieTags.push(genreTags.find((tag) => id === tag.id).name);
+                });
                 return (
-                    <MovieCard imageURL={"https://image.tmdb.org/t/p/w500" + movie.poster_path} title={movie.title} subtitle={movie.release_date} tags={movie.genre_ids} rating={movie.vote_average} voteCount={movie.vote_count} />
+                    <MovieCard imageURL={"https://image.tmdb.org/t/p/w500" + movie.poster_path} title={movie.title} subtitle={movie.release_date} tags={movieTags} rating={movie.vote_average} voteCount={movie.vote_count} />
                 )
             })}
         </div>
