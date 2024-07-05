f84b0c19cda525e4745dec7d88a156f1082b0a71
adim2222 <121557893+adim2222@users.noreply.github.com>
2024-07-05 22:38:54 +0200

    Swapped Routes to Switch

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,15 @@
-import { HashRouter, Routes, Route} from "react-router-dom";
+import { HashRouter, Switch, Route} from "react-router-dom";
 
 export const Router = () => {
     return (
         <HashRouter>
-            <Routes>
+            <Switch>
                 <Route path="/movies" element={"Filmy"}/>
                 <Route path="/movies/:id" element={"Film"} />
                 <Route path="/people/:id" element={"Osoba"} />
                 <Route path="/people" element={"Osoby"} />
                 <Route path="/" element={"Strona główna"} />
-            </Routes>
+            </Switch>
         </HashRouter>
     )
 };
