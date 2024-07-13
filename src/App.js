00b12011156b0b71b6d08ceb74bd52fa19012e1a
Adam Nowak <nowak.adam2222@gmail.com>
2024-07-13 17:01:38 +0200

    fixed context error

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import { Router } from "./Routes/router";
-import NavigationBar from "./common/Navigation";
 
 function App() {
     return (
         <div className="App">
-            <NavigationBar />
             <Router />
         </div>
     );
