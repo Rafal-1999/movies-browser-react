603a0b04d9710523d469b08f2adbad1601071e8f
Adam Nowak <nowak.adam2222@gmail.com>
2024-07-24 21:22:53 +0200

    finishing data structure and operations + movie header

---
+++ src/Routes/MoviePage/components/MovieHeadingStyled.js
@@ -0,0 +1,50 @@
+import styled from "styled-components"
+
+export const Container = styled.div`
+    background-color: black; 
+    width: 100%;
+    aspect-ratio: 192/77;
+    display: flex;
+    justify-content: center;
+    background: linear-gradient(189.44deg, rgba(0, 0, 0, 0) 58.48%, rgba(0, 0, 0, 0.106473) 63.17%, rgba(0, 0, 0, 0.235359) 68.85%, rgba(0, 0, 0, 0.492821) 78.08%, rgba(0, 0, 0, 0.740286) 85.86%, #000000 92.87%),
+linear-gradient(180deg, #000000 0%, rgba(0, 0, 0, 0.689555) 4.66%, rgba(0, 0, 0, 0.439033) 9.34%, rgba(0, 0, 0, 0.20628) 15.16%, rgba(0, 0, 0, 0) 24.22%),
+linear-gradient(90deg, #000000 13.6%, rgba(0, 0, 0, 0.984059) 14.58%, rgba(0, 0, 0, 0.967732) 15.44%, rgba(0, 0, 0, 0.865569) 16.3%, rgba(0, 0, 0, 0.230315) 22.87%, rgba(0, 0, 0, 0) 26.64%),
+linear-gradient(270deg, #000000 14.11%, rgba(0, 0, 0, 0.873268) 15.08%, rgba(0, 0, 0, 0.720529) 16.51%, rgba(0, 0, 0, 0.294577) 19.99%, rgba(0, 0, 0, 0.159921) 21.88%, rgba(0, 0, 0, 0) 25.68%);
+`
+
+export const Banner = styled.img`
+    height: 100%;
+    z-index: -1;
+`
+
+export const Details = styled.div`
+    align-self: flex-end;
+    justify-self: left;
+    position: absolute;
+    color: white;
+    margin-right: 50%;
+`
+
+export const Title = styled.p`
+    font-weight: 600;
+    font-size: 64px;
+    margin: 0px;
+`
+
+export const Rating = styled.div`
+    font-size: 30px;
+    font-weight: 500;
+`
+
+export const OutOf = styled.span`
+    font-weight: 400;
+    font-size: 14px;
+`
+
+export const Stats = styled.div`
+    margin-left: 8px;
+`
+
+export const Votes = styled.div`
+    margin-bottom: 56px;
+`
--- src/Routes/MoviePage/index.js
+++ src/Routes/MoviePage/index.js
@@ -1,5 +1,6 @@
-import { MovieHeading, MovieInfo, Cast, Crew, CastHeader, CrewHeader, CastList, CrewList } from "./styled";
+import { MovieInfo, Cast, Crew, CastHeader, CrewHeader, CastList, CrewList } from "./styled";
 import MovieCardFull from "../../common/MovieCardFull"
+import MovieHeading from "./components/MovieHeading.js"
 import { useParams } from "react-router-dom";
 import { useDataById } from "../../useDataById";
 import { useSelector } from "react-redux";
@@ -17,11 +18,32 @@ const MoviePage = () => {
     const cast = useSelector(selectCast);
     const crew = useSelector(selectCrew);
 
+    // these if statements are kinda bad but i didnt know any other way of making operations on selected variables :C
+
+    let rating = 0;
+    if (movie.vote_average) {
+        rating = movie.vote_average.toFixed(1);
+    }
+
+    let tags = [];
+    if (movie.genres) {
+        movie.genres.forEach(genre => {
+            tags.push(genre.name)
+        })
+    }
+
+    let production = "";
+    if (movie.production_companies) {
+        movie.production_companies.forEach(company => {
+            production += company.name;
+        })
+    }
+
     return (
         <>
-            <MovieHeading/>
+            <MovieHeading background={"https://image.tmdb.org/t/p/w500" + movie.backdrop_path} title={movie.title} rating={rating} voteCount={movie.vote_count}/>
             <MovieInfo>
-                <MovieCardFull description={movie.overview} imageURL={"https://image.tmdb.org/t/p/w500" + movie.poster_path} title={movie.title} production={"asdf"} release={movie.release_date} tags={["Asdf"]} rating={movie.vote_average} voteCount={movie.vote_count} />
+                <MovieCardFull description={movie.overview} imageURL={"https://image.tmdb.org/t/p/w500" + movie.poster_path} title={movie.title} production={production} release={movie.release_date} tags={tags} rating={movie.vote_average} voteCount={movie.vote_count} />
             </MovieInfo>
             <Cast>
                 <CastHeader>Cast</CastHeader>
