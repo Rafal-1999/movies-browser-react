faa896ffc5635ce459a0b33d8ca16753bc1d033f
Rafal Chudy <rafalchu5@gmail.com>
2024-07-31 22:24:44 +0200

    Changed placeholder in input for pages

--- src/common/Navigation/index.js
+++ src/common/Navigation/index.js
@@ -1,17 +1,30 @@
+import { useLocation } from "react-router-dom";
 import { VideoIcon, NavigationBar, MainHeader, Box, BoxNav, LinkNavigation, Input } from "./styled";
 import { toMovieCard } from "../../Routes/router";
 
-const Navigation = ({query}) => (
-    <NavigationBar>
-        <Box>
-            <MainHeader><VideoIcon />Movies Browser</MainHeader>
-            <BoxNav>
-                <LinkNavigation to={toMovieCard()}>MOVIES</LinkNavigation>
-                <LinkNavigation to="/people">PEOPLE</LinkNavigation>
-            </BoxNav>
-            <Input placeholder={"Search for movies..."} type="text" value={query} onChange={""} />
-        </Box>
-    </NavigationBar>
-);
+const Navigation = ({ query }) => {
+    const path = useLocation();
+    console.log(path);
+
+    let content = "movies";
+    if (path.pathname.includes("movies")) {
+        content = "movies";
+    } else {
+        content = "people";
+    }
+
+    return (
+        <NavigationBar>
+            <Box>
+                <MainHeader><VideoIcon />Movies Browser</MainHeader>
+                <BoxNav>
+                    <LinkNavigation to={toMovieCard()}>MOVIES</LinkNavigation>
+                    <LinkNavigation to="/people">PEOPLE</LinkNavigation>
+                </BoxNav>
+                <Input placeholder={`Search for ${content}...`} type="text" value={query} onChange={""} />
+            </Box>
+        </NavigationBar>
+    )
+};
 
 export default Navigation;
