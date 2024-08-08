7ae1370b5e07183a792a400b29166d1f2e1a3695
Dawid Galeski <dawidgaski@gmail.com>
2024-08-08 23:51:13 +0200

    fixes

--- src/common/Navigation/index.js
+++ src/common/Navigation/index.js
@@ -6,53 +6,57 @@ import {
   Box,
   BoxNav,
   LinkNavigation,
-  Input
+  Input,
 } from "./styled";
-import { toPopularMovies } from "../../Routes/router";
+import { toPopularMovies, toPopularPeople } from "../../Routes/router";
 import { useSearchParams, useNavigate } from "react-router-dom";
 import { useRef } from "react";
 
 const Navigation = () => {
-    const path = useLocation();
-  
-    let content = "movies";
-    if (path.pathname.includes("movies")) {
-        content = "movies";
-    } else {
-        content = "people";
-    }
+  const path = useLocation();
 
-    const [params, setParams] = useSearchParams();
-    const searchRef = useRef();
-    const navigate = useNavigate();
+  let content = "movies";
+  if (path.pathname.includes("movies")) {
+    content = "movies";
+  } else {
+    content = "people";
+  }
 
-    let timeout = 0;
-    const searchbarHandler = () => {
-        setParams(`query=${searchRef.current.value}`)
-        clearTimeout(timeout);
-        timeout = setTimeout(() => {
-            navigate(`/search?query=${searchRef.current.value}`);
-        }, 3000);
-    };
+  const [params, setParams] = useSearchParams();
+  const searchRef = useRef();
+  const navigate = useNavigate();
 
-    return (
-        <NavigationBar>
-            <Box>
-                <MainHeader><VideoIcon />Movies Browser</MainHeader>
-                <BoxNav>
-                    <LinkNavigation to={toPopularMovies()}>MOVIES</LinkNavigation>
-                    <LinkNavigation to={toPopularPeople()}>PEOPLE</LinkNavigation>
-                </BoxNav>
-                <Input 
-                    placeholder={`Search for ${content}...`}
-                    type="text"
-                    value={query}
-                    ref={searchRef}
-                    onChange={() => {searchbarHandler()}}
-                />
-            </Box>
-        </NavigationBar>     
-    )
+  let timeout = 0;
+  const searchbarHandler = () => {
+    setParams(`query=${searchRef.current.value}`);
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      navigate(`/search?query=${searchRef.current.value}`);
+    }, 3000);
+  };
+
+  return (
+    <NavigationBar>
+      <Box>
+        <MainHeader>
+          <VideoIcon />
+          Movies Browser
+        </MainHeader>
+        <BoxNav>
+          <LinkNavigation to={toPopularMovies()}>MOVIES</LinkNavigation>
+          <LinkNavigation to={toPopularPeople()}>PEOPLE</LinkNavigation>
+        </BoxNav>
+        <Input
+          placeholder={`Search for ${content}...`}
+          type="text"
+          ref={searchRef}
+          onChange={() => {
+            searchbarHandler();
+          }}
+        />
+      </Box>
+    </NavigationBar>
+  );
 };
 
 export default Navigation;
